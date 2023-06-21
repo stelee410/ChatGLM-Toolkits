ac0a42289eb872b262b65b7f8aa6ab9e6325d564
stelee410 <129747297+stelee410@users.noreply.github.com>
2023-06-21 23:11:07 +0800

    add new pair question

--- lora.py
+++ lora.py
@@ -3,4 +3,4 @@ from infering import get_peft_model, ChatBot
 
 model, tokenizer = get_peft_model(MODEL_PATH, LORA_PATH)
 chatbot = ChatBot(model, tokenizer)
-print(chatbot.predict("你好"))
+print(chatbot.predict("小李体检发现P-R间期延长，伴有T波低平和ST段异常，应该怎么办"))
