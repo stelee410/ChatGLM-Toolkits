a1297395df3dc37b8b462af240052c1073aece09
stelee410 <129747297+stelee410@users.noreply.github.com>
2023-06-28 16:25:15 +0800

    add chatbox support

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import config
+from config import MODEL_PATH, LORA_PATH
+from infering import get_peft_model, ChatBot
+
 if __name__ == "__main__":
     print("=== traing by pre-config ===")
+    model, tokenizer = get_peft_model(MODEL_PATH, LORA_PATH)
+    chatbot = ChatBot(model, tokenizer)
+    while True:
+        q = input(">>> ")
+        if q == 'q!':
+            break
+        else:
+            print(chatbot.predict(q))
     
